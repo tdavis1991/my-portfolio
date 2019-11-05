8f8a11f32e993310aaac248e00bdc211e40a9bf3
tevin davis <aod.td1105@gmail.com>
2019-11-04 20:52:01 -0500

    added comment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Tevin's portfolio
+          Welcome to Tevin's portfolio. Future software engineer
         </p>
       </header>
     </div>
